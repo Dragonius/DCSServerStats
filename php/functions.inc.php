012201c94fef2d02433a35a6b152247b2b9ba523
Dragonius <Blackerhawker@hotmail.com>
2024-06-21 11:50:15 +0300

    added marker to map. Lot way to go as not compatilbe php 8.23, as it is NOT tested
    (cherry picked from commit 5d5ee22848cafcc414fc23c6bb0ee9c5382cc8b8)

--- php/functions.inc.php
+++ php/functions.inc.php
@@ -547,7 +547,7 @@ class SimStats {
 	
 	public function echoLiveRadarMapScript() {
 		echo "<br>
-		<a href='#' onclick=\"setMapCenter([42.858056, 41.128056]);setMapZoom(7);\">Caucasus</a>- 
+		<a href='#' onclick=\"setMapCenter([42.858056, 41.128056]);setMapZoom(7);\">Caucasus</a> - 
 		<a href='#' onclick=\"setMapCenter([38.18638677, -115.16967773]);setMapZoom(7);\">Nevada</a>
 		<br>";
 		
@@ -556,11 +556,29 @@ class SimStats {
 		echo "<script src=\"https://unpkg.com/leaflet/dist/leaflet.js\"></script>";
 		// Initialize the map
 		echo "<script>";
-		echo "var map = L.map('map').setView([42.858056, 41.128056], 7);"; // Initial map center and zoom level
-		echo "L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {";
-		echo "attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'";
-		echo "}).addTo(map);";
+        echo "var map = L.map('map').setView([42.858056, 41.128056], 7);"; // Initial map center and zoom level
+//		echo "const map = L.map('map', {
+//		crs: L.CRS.Simple,
+//		minZoom: 0
+//	}).setView([-50.00, 70.00], 3);";
+
+//	const bounds = [[-50.0,55], [1021.5,1023]];
+		echo "var zero = L.latLng([ 42.8, 41.1 ]);";
+//	const hund = L.latLng([-100,100]);
+//  const hand = L.latLng([-200,200]);
+
+		echo "L.marker(zero).addTo(map);";
+//	L.marker(hund).addTo(map);
+//	L.marker(hand).addTo(map);        
+		echo "const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            minZoom: 0,
+            maxZoom: 17,
+			//bounds,
+			//edgeBufferTiles: 1,
+			attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors',
+        }).addTo(map);";
 		echo "</script>";
+	echo "<br> TEST <br>";
 	}
 	
 	public function echoMapScriptForFlight($flightid) {
@@ -574,13 +592,14 @@ class SimStats {
 		// Leaflet does not require an API key
 		echo "<script src=\"https://unpkg.com/leaflet/dist/leaflet.js\"></script>";
 		// Initialize the map and set flight path
-		echo "<script>";
-		echo "var map = L.map('map').setView([42.858056, 41.128056], 6);"; // Initial map center and zoom level
-		echo "L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {";
-		echo "attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'";
-		echo "}).addTo(map);";
-		echo "setFlightPath(map, " . $flightid . ");"; // Function to set flight path
-		echo "</script>";
+//		echo "<script>";
+//		echo "var map = L.map('map').setView([42.858056, 41.128056], 6);"; // Initial map center and zoom level
+//		echo "L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {";
+//		echo "attribution: '&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors'";
+//		echo "}).addTo(map);";
+//		echo "setFlightPath(map, " . $flightid . ");"; // Function to set flight path
+//		echo "</script>";
+		
 	}
 	
 	public function getLiveRadarMapInfoJSON() {
